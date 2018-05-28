1f7c4a5b2d822814e2db988b555a0b870cfaef31
taylorj21 <39700273+taylorj21@users.noreply.github.com>
2018-05-28 13:54:43 +0100

    Add files via upload

---
+++ OpenCVTests.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+
+vidCap = cv2.VideoCapture(0)
+ret, frame = vidCap.read()
+height, width, _ = frame.shape
+
+while True:
+    ret, frame = vidCap.read()
+    # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #frame = cv2.line(frame, (0,0), (100, 100),(255,0,0),5)
+    frame = cv2.circle(frame, (int(width/2), int(height/2)), int(height/2), (255,0,0),5)
+    #frame = cv2.rectangle(frame, (10, 10), (30, 30), (0,255,0), 10)
+    frame = cv2.flip(frame, 1)
+    cv2.imshow('Circle', frame)
+
+    if cv2.waitKey(20) == 27:
+        break
+
+vidCap.release()
+cv2.destroyAllWindows()
